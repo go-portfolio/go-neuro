370b86acd4f3d74ab4a156610f0f47882f6986b7
aleksey <av.pavlow@gmail.com>
2025-11-14 08:49:03 +0300

    feat: Добавить сохранение модели в файл и загрузку модели из файла - Сохранение и згрузка модели из файла добвлена

--- cmd/main.go
+++ cmd/main.go
@@ -3,34 +3,50 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/rand"
-	"time"
-
 	"github.com/go-portfolio/go-neuro/nn"
 	"github.com/go-portfolio/go-neuro/nn/dataset"
 	"github.com/go-portfolio/go-neuro/nn/trainers"
 )
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	// 1. Берём датасет
+	ds := dataset.MustGet("xor")
 
-	// Выбираем датасет
-	ds := dataset.MustGet("and")
-
-	// Архитектура нейросети
+	// 2. Создаём сеть
 	net := nn.NewNetwork([]int{2, 2, 1})
 
-	// Выбираем алгоритм обучения
+	// 3. Выбираем тренер
 	trainer := &trainers.SGDTrainer{LearningRate: 0.5}
 
-	// Обучение
+	// 4. Обучаем сеть
 	net.Fit(trainer, ds.Samples, ds.Targets, 20000)
 
-	// Тест
-	fmt.Println("\nРезультаты:")
+	fmt.Println("\nTraining done!")
+
+	// 5. Сохраняем модель
+	err := net.SaveModel("xor_model.json")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Model saved → xor_model.json")
+
+	// 6. Загружаем модель
+	loadedNet, err := nn.LoadModel("xor_model.json")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Model loaded!")
+
+	// 7. Проверяем предсказания загруженной сети
+	fmt.Println("\n--- Prediction check ---")
 	for i, s := range ds.Samples {
-		out := net.Predict(s)
-		fmt.Printf("Вход: %v → %.4f (округлён %d), Цель: %v\n",
-			s, out[0], int(math.Round(out[0])), ds.Targets[i])
+		out := loadedNet.Predict(s)
+		fmt.Printf(
+			"Input: %v → Output: %.4f (rounded %d), Target: %v\n",
+			s,
+			out[0],
+			int(math.Round(out[0])),
+			ds.Targets[i],
+		)
 	}
 }
